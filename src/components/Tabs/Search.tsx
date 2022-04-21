4fe938dc98e028fd081092cef3f4b2c99d241f39
joseferrar <joseferrar4@gmail.com>
2022-04-21 18:53:20 +0530

    search page design

--- src/components/Tabs/Search.tsx
+++ src/components/Tabs/Search.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import notebook from "../../images/notebook.png";
+import ellipsis from "../../images/Ellipsis.png";
+import { getData } from "../../data/dummyData";
 import SearchInput from "../Inputs/SearchInput";
 import SideLayout from "../Layouts/SideLayout";
 
 function Search() {
+  const data = getData();
   return (
     <SideLayout>
       <SearchInput />
@@ -31,6 +39,84 @@ function Search() {
           }}
         />
       </Stack>
+
+      {data.map(
+        (item, index) =>
+          index > 3 && (
+            <Fragment key={index}>
+              {item.desc.map((sub, i) => (
+                <Grid
+                  key={i}
+                  style={{
+                    boxShadow: "rgb(241 241 245) 5px 5px 5px 5px",
+                    marginLeft: 12,
+                    marginRight: 12,
+                    borderStyle: "solid",
+                    borderRadius: 12,
+                    borderWidth: 4,
+                    marginTop: 12,
+                    backgroundColor: "#fff",
+                    borderColor: "#fff",
+                    padding: 4,
+                  }}
+                >
+                  <Grid
+                    style={{
+                      display: "flex",
+                      marginBottom: 10,
+                      paddingLeft: 8,
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      style={{
+                        textAlign: "left",
+                        fontSize: 12,
+                        fontFamily: "DMSans-Medium",
+                        width: 340,
+                      }}
+                    >
+                      {Object.values(sub).map((a) => a)}
+                    </Typography>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={ellipsis}
+                      sx={{ width: 25, height: 25, marginLeft: "auto" }}
+                    />
+                  </Grid>
+                  <Divider />
+                  <Button
+                    variant="outlined"
+                    startIcon={
+                      <Avatar
+                        alt="Remy Sharp"
+                        src={notebook}
+                        sx={{ width: 15, height: 15 }}
+                      />
+                    }
+                    style={{
+                      userSelect: "none",
+                      borderRadius: 16,
+                      color: "gray",
+                      borderColor: "gray",
+                      textTransform: "capitalize",
+                      display: "flex",
+                      marginTop: 12,
+                      height: 32,
+                      marginBottom: 12,
+                      WebkitTouchCallout: "none",
+                      marginLeft: 8,
+                      fontSize: 12,
+                      fontFamily: "DMSans-Regular",
+                    }}
+                  >
+                    Note Block
+                  </Button>
+                </Grid>
+              ))}
+            </Fragment>
+          )
+      )}
     </SideLayout>
   );
 }
